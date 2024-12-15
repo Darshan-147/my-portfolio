309aa58e0cd30d5e8dfc5772e78d1a11aa635012
Darshan <darshaner2019@gmail.com>
2024-12-15 11:29:16 +0530

    updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "hover:border-orange-300",
+    "hover:border-blue-300",
+    "hover:border-green-400",
+    "hover:border-gray-400",
+    "hover:border-cyan-200",
+    "hover:border-green-300",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
